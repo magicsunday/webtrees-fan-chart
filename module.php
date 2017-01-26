b4e08022c385130d38d2f2a296aad0dacea21ed5
Rico Sonntag <rico.sonntag@netresearch.de>
2017-01-26 13:55:02 +0100

    Added module and chart controller

---
+++ module.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Webtrees module.
+ *
+ * PHP version 5
+ *
+ * @category   Webtrees
+ * @package    Module
+ * @subpackage AncestralFanChart
+ * @author     Rico Sonntag <mail@ricosonntag.de>
+ * @license    TBD
+ * @link       https://github.com/magicsunday/ancestral-fan-chart/
+ */
+
+namespace RSO\WebtreesModule\AncestralFanChart;
+
+use Composer\Autoload\ClassLoader;
+use Fisharebest\Webtrees\Auth;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Individual;
+use Fisharebest\Webtrees\Menu;
+use Fisharebest\Webtrees\Module;
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Fisharebest\Webtrees\Module\ModuleChartInterface;
+
+use RSO\WebtreesModule\AncestralFanChart\Controller\Chart;
+
+/**
+ * Ancestral fan chart module class.
+ *
+ * @category   Webtrees
+ * @package    Module
+ * @subpackage AncestralFanChart
+ * @author     Rico Sonntag <mail@ricosonntag.de>
+ * @license    TBD
+ * @link       https://github.com/magicsunday/ancestral-fan-chart/
+ */
+class AncestralFanChartModule extends AbstractModule implements ModuleChartInterface
+{
+    /**
+     * Create a new module.
+     *
+     * @param string $directory Where is this module installed
+     */
+    public function __construct($directory)
+    {
+        parent::__construct($directory);
+
+        //$this->action = Filter::get('mod_action');
+
+        // Register the namespace
+        $loader = new ClassLoader();
+        $loader->addPsr4(
+            'RSO\\WebtreesModule\\AncestralFanChart\\',
+            WT_MODULES_DIR . $this->getName() . '/src'
+        );
+        $loader->register();
+    }
+
+    /**
+     * How should this module be labelled on tabs, menus, etc.?
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return I18N::translate('Ancestral fan chart');
+    }
+
+    /**
+     * A sentence describing what this module does.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return I18N::translate('A fan chart of an individual’s ancestors.');
+    }
+
+    /**
+     * What is the default access level for this module?
+     *
+     * Some modules are aimed at admins or managers, and are not generally shown to users.
+     *
+     * @return int
+     */
+    public function defaultAccessLevel()
+    {
+        return Auth::PRIV_PRIVATE;
+    }
+
+    /**
+     * Return a menu item for this chart.
+     *
+     * @param Individual $individual Current individual instance
+     *
+     * @return Menu
+     */
+    public function getChartMenu(Individual $individual)
+    {
+        $link = 'module.php?mod=' . $this->getName()
+            . '&amp;rootid=' . $individual->getXref()
+            . '&amp;ged=' . $individual->getTree()->getNameUrl();
+
+        return new Menu(
+            $this->getTitle(),
+            $link,
+            'menu-chart-ancestral-fan-chart',
+            array(
+                'rel' => 'nofollow',
+            )
+        );
+    }
+
+    /**
+     * Return a menu item for this chart - for use in individual boxes.
+     *
+     * @param Individual $individual Current individual instance
+     *
+     * @return Menu
+     */
+    public function getBoxChartMenu(Individual $individual)
+    {
+        return $this->getChartMenu($individual);
+    }
+
+    /**
+     * Get the modules static url path.
+     *
+     * @return string
+     */
+    protected function getModuleUrlPath()
+    {
+        return WT_STATIC_URL . WT_MODULES_DIR . $this->getName();
+    }
+
+    /**
+     * This is a general purpose hook, allowing modules to respond to routes
+     * of the form module.php?mod=FOO&mod_action=BAR
+     *
+     * @param string $mod_action
+     *
+     * @return void
+     */
+    public function modAction($mod_action)
+    {
+        global $controller;
+        global $WT_TREE;
+
+        $urlPath = $this->getModuleUrlPath();
+
+        $controller = new Controller\Chart();
+        $controller
+            ->restrictAccess(Module::isActiveChart($WT_TREE, 'ancestral-fan-chart'))
+            ->pageHeader()
+            ->addExternalJavascript($urlPath . '/js/packages/d3-3.5.17/d3.min.js')
+            ->addExternalJavascript($urlPath . '/js/ancestral-fan-chart.js');
+
+        echo '<link rel="stylesheet" type="text/css" href="'
+            . $urlPath . '/css/ancestral-fan-chart.css">';
+
+        echo $controller->render();
+    }
+}
+
+return new AncestralFanChartModule(__DIR__);
+?>
---
+++ src/Controller/Chart.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Webtrees module.
+ *
+ * PHP version 5
+ *
+ * @category   Webtrees
+ * @package    Module
+ * @subpackage Controller
+ * @author     Rico Sonntag <mail@ricosonntag.de>
+ * @license    TBD
+ * @link       https://github.com/magicsunday/ancestral-fan-chart/
+ */
+
+namespace RSO\WebtreesModule\AncestralFanChart\Controller;
+
+/**
+ * Fan chart controller class.
+ *
+ * @category   Webtrees
+ * @package    Module
+ * @subpackage Controller
+ * @author     Rico Sonntag <mail@ricosonntag.de>
+ * @license    TBD
+ * @link       https://github.com/magicsunday/ancestral-fan-chart/
+ */
+class Chart extends \Fisharebest\Webtrees\Controller\ChartController
+{
+    /**
+     * Number of generations to display.
+     *
+     * @var int
+     */
+    public $generations = 8;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+         parent::__construct();
+
+         if ($this->root && $this->root->canShowName()) {
+             $this->setPageTitle(
+                 \Fisharebest\Webtrees\I18N::translate(
+                    'Ancestral fan chart of %s', $this->root->getFullName()
+                 )
+             );
+         } else {
+             $this->setPageTitle(
+                 \Fisharebest\Webtrees\I18N::translate('Ancestral fan chart')
+             );
+         }
+    }
+
+    /**
+     * Get the default colors based on the gender of an individual.
+     *
+     * @param \Fisharebest\Webtrees\Individual $person Individual instance
+     *
+     * @return string HTML color code
+     */
+    protected function getColor(\Fisharebest\Webtrees\Individual $person = null)
+    {
+        if (!($person instanceof \Fisharebest\Webtrees\Individual)) {
+            return '#fff';
+        }
+
+        if ($person->getSex() === 'M') {
+            return '#b1cff0';
+        } elseif ($person->getSex() === 'F') {
+            return '#e9daf1';
+        }
+
+        return '#fff';
+    }
+
+    /**
+     * Get the individual data required for display the chart.
+     *
+     * @param int                              $generation Current generation
+     * @param \Fisharebest\Webtrees\Individual $person     Start person
+     *
+     * @return array
+     */
+    protected function getIndividualData(
+        \Fisharebest\Webtrees\Individual $person = null, $generation = 1
+    ) {
+        // We need always two individuals, so we fake the missing ones
+        if (!($person instanceof \Fisharebest\Webtrees\Individual)) {
+            return array(
+                'id'         => '',
+                'generation' => $generation,
+                'name'       => '',
+                'born'       => '',
+                'died'       => '',
+                'color'      => $this->getColor(),
+                'children'   => array(),
+            );
+        }
+
+        return array(
+            'id'         => $person->getXref(),
+            'generation' => $generation,
+            'name'       => strip_tags($person->getFullName()),
+            'born'       => $person->getBirthYear(),
+            'died'       => $person->getDeathYear(),
+            'color'      => $this->getColor($person),
+            'children'   => array(),
+        );
+    }
+
+    /**
+     * Recursively build the data array of the individual ancestors.
+     *
+     * @param \Fisharebest\Webtrees\Individual $person     Start person
+     * @param int                              $generation Current generation
+     *
+     * @return array
+     *
+     * @todo Rebuild this to a iterative method
+     */
+    protected function buildJsonTree(
+        \Fisharebest\Webtrees\Individual $person = null, $generation = 1
+    ) {
+        // Maximum generation reached
+        if ($generation === $this->generations) {
+            return array();
+        }
+
+        $json   = $this->getIndividualData($person, $generation);
+        $family = null;
+
+        if ($person instanceof \Fisharebest\Webtrees\Individual) {
+            $family = $person->getPrimaryChildFamily();
+        }
+
+        if ($family instanceof \Fisharebest\Webtrees\Family) {
+            $father = $family->getHusband();
+            $mother = $family->getWife();
+        } else {
+            $father = null;
+            $mother = null;
+        }
+
+        // Recursively call the method for the parents of the individual
+        $fatherTree = $this->buildJsonTree($father, $generation + 1);
+        $motherTree = $this->buildJsonTree($mother, $generation + 1);
+
+        if ($fatherTree) {
+            $json['children'][] = $fatherTree;
+        }
+
+        if ($motherTree) {
+            $json['children'][] = $motherTree;
+        }
+
+        return $json;
+    }
+
+    /**
+     * Render the fan chart HTML and JSON data.
+     *
+     * @return string HTML snippet to include in page HTML
+     */
+    public function render()
+    {
+        // Encode data to json string
+        $json = json_encode(
+            $this->buildJsonTree($this->root)
+        );
+
+        // Add the json right before any module included js
+        $this->addInlineJavascript(
+            'var chartData = ' . $json . ';',
+            self::JS_PRIORITY_HIGH
+        );
+
+        $title = 'Ancestral fan chart for '
+            . strip_tags($this->root->getFullName());
+
+        return <<<HTML
+<div id="ancestral-fan-chart">
+    <div>
+        <h1>{$title}</h1>
+    </div>
+    <div id="fan-chart"></div>
+</div>
+HTML;
+    }
+}
+?>
