3601bf9b7f504fac970895ea0c65a4582a592693
Rico Sonntag <mail@ricosonntag.de>
2017-01-29 12:10:24 +0100

    Added initial form to allow configuration of fan chart

--- src/Controller/Chart.php
+++ src/Controller/Chart.php
@@ -32,6 +32,10 @@ use Fisharebest\Webtrees\Controller\ChartController;
 use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\Individual;
 use Fisharebest\Webtrees\Family;
+use Fisharebest\Webtrees\Tree;
+use Fisharebest\Webtrees\Filter;
+use Fisharebest\Webtrees\Functions\FunctionsEdit;
+use Fisharebest\Webtrees\Functions\FunctionsPrint;
 
 /**
  * Fan chart controller class.
@@ -50,7 +54,21 @@ class Chart extends ChartController
      *
      * @var int
      */
-    protected $generations = 7;
+    protected $generations = 5;
+
+    /**
+     * Style of fan chart. (2 = full circle, 3, three-quarter circle, 4 = half circle)
+     *
+     * @var int
+     */
+    protected $fanStyle = 3;
+
+    /**
+     * Width of fanchart (a percentage).
+     *
+     * @var int
+     */
+    protected $fanWidth;
 
     /**
      * Constructor.
@@ -58,6 +76,14 @@ class Chart extends ChartController
     public function __construct()
     {
          parent::__construct();
+
+
+         $defaultGenerations = $this->getTree()->getPreference('DEFAULT_PEDIGREE_GENERATIONS');
+
+         // Extract the request parameters
+         $this->fanStyle    = Filter::getInteger('fanStyle', 2, 4, 3);
+//          $this->fanWidth    = Filter::getInteger('fanWidth', 50, 500, 100);
+         $this->generations = Filter::getInteger('generations', 2, 9, $defaultGenerations);
 
          if ($this->root && $this->root->canShowName()) {
              $this->setPageTitle(
@@ -178,6 +204,56 @@ class Chart extends ChartController
     }
 
     /**
+     *
+     * @return Tree
+     */
+    protected function getTree()
+    {
+        global $WT_TREE;
+        return $WT_TREE;
+    }
+
+    /**
+     *
+     * @param string $value String to translate
+     *
+     * @return string
+     */
+    protected function translate($value)
+    {
+        return I18N::translate($value);
+    }
+
+    protected function printFindIndividualLink()
+    {
+        return FunctionsPrint::printFindIndividualLink('rootid');
+    }
+
+    protected function selectEditControl($name, $values, $empty, $selected, $extra = '')
+    {
+        return FunctionsEdit::selectEditControl('fanStyle', $this->getFanStyles(), null, $this->fanStyle);
+    }
+
+    protected function editFieldInteger($name, $selected = '', $min, $max, $extra = '')
+    {
+        return FunctionsEdit::editFieldInteger('generations', $this->generations, 2, 9);
+    }
+
+    /**
+     * A list of options for the chart style.
+     *
+     * @return string[]
+     */
+    protected function getFanStyles()
+    {
+        return [
+            4 => $this->translate('half circle'),
+            3 => $this->translate('three-quarter circle'),
+            2 => $this->translate('full circle'),
+        ];
+    }
+
+    /**
      * Render the fan chart HTML and JSON data.
      *
      * @return string HTML snippet to include in page HTML
@@ -201,6 +277,7 @@ $(function () {
 
     if (typeof $().ancestralFanChart === 'function') {
         fanChart.ancestralFanChart({
+            fanStyle : {$this->fanStyle},
             data : {$json}
         });
     }
@@ -208,14 +285,55 @@ $(function () {
 JS
         );
 
-        $title = 'Ancestral fan chart for '
-            . strip_tags($this->root->getFullName());
-
         return <<<HTML
 <div id="ancestral-fan-chart">
-    <div>
-        <h1>{$title}</h1>
-    </div>
+    <h2>{$this->getPageTitle()}</h2>
+    <form name="people" method="get" action="?">
+        <input type="hidden" name="ged" value="{$this->getTree()->getNameHtml()}">
+        <input type="hidden" name="mod" value="ancestral-fan-chart">
+        <table class="list_table">
+            <tbody>
+                <tr>
+                    <td class="descriptionbox">
+                        <label for="rootid">{$this->translate('Individual')}</label>
+                    </td>
+                    <td class="optionbox">
+                        <input class="pedigree_form" data-autocomplete-type="INDI" type="text" name="rootid" id="rootid" size="3" value="{$this->root->getXref()}">
+                        {$this->printFindIndividualLink('rootid')}
+                    </td>
+                    <td class="descriptionbox">
+                        <label for="fanStyle">
+                            {$this->translate('Layout')}
+                            </label>
+                    </td>
+                    <td class="optionbox">
+                        {$this->selectEditControl('fanStyle', $this->getFanStyles(), null, $this->fanStyle)}
+                    </td>
+                    <td rowspan="2" class="topbottombar vmiddle">
+                        <input type="submit" value="{$this->translate('view')}">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="descriptionbox">
+                        <label for="generations">
+                            {$this->translate('Generations')}
+                        </label>
+                    </td>
+                    <td class="optionbox">
+                        {$this->editFieldInteger('generations', $this->generations, 2, 9)}
+                    </td>
+                    <td class="descriptionbox">
+                        <label for="fanWidth">
+                            {$this->translate('Zoom')}
+                        </label>
+                    </td>
+                    <td class="optionbox">
+                        <input type="text" size="3" id="fanWidth" name="fanWidth" value="{$this->fanWidth}"> %
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
     <div id="fan-chart"></div>
 </div>
 HTML;
