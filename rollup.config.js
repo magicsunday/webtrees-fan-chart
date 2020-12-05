1eebeb8a5ffc104765108cbb4462769cd72681a5
Rico <mail@ricosonntag.de>
2020-12-05 12:01:54 +0100

    Updated rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,39 @@
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
 
-export default {
-    input: "resources/js/modules/index.js",
-    output: [
-        {
-            name: "WebtreesFanChart",
-            file: "resources/js/fan-chart.js",
-            format: "umd"
-        },
-        {
-            name: "WebtreesFanChart",
-            file: "resources/js/fan-chart.min.js",
-            format: "umd"
-        }
-    ],
-    plugins: [
-        resolve(),
-        terser({
-            mangle: true,
-            compress: true,
-            module: true,
-            output: {
-                comments: false
+export default [
+    {
+        input: "resources/js/modules/index.js",
+        output: [
+            {
+                name: "WebtreesFanChart",
+                file: "resources/js/fan-chart.js",
+                format: "umd"
             }
-        })
-    ]
-};
+        ],
+        plugins: [
+            resolve()
+        ]
+    },
+    {
+        input: "resources/js/modules/index.js",
+        output: [
+            {
+                name: "WebtreesFanChart",
+                file: "resources/js/fan-chart.min.js",
+                format: "umd"
+            }
+        ],
+        plugins: [
+            resolve(),
+            terser({
+                mangle: true,
+                compress: true,
+                module: true,
+                output: {
+                    comments: false
+                }
+            })
+        ]
+    }
+];
