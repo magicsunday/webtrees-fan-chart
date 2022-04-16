e6757e29e787b45de23d2a1db766f4e2de9d4f9c
Rico Sonntag <mail@ricosonntag.de>
2022-04-16 19:14:34 +0200

    Release 2.3.0

--- src/Module.php
+++ src/Module.php
@@ -60,7 +60,7 @@ class Module extends AbstractModule implements ModuleCustomInterface, ModuleChar
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.2.3-dev';
+    public const CUSTOM_VERSION = '2.3.0';
 
     /**
      * @var string
