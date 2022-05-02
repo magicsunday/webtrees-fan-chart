5c5f4db0cbfd14d8f030c3e4812232508d0f4346
Rico Sonntag <mail@ricosonntag.de>
2022-05-02 10:20:37 +0200

    2.4.1-dev

--- src/Module.php
+++ src/Module.php
@@ -60,7 +60,7 @@ class Module extends AbstractModule implements ModuleCustomInterface, ModuleChar
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.4.0';
+    public const CUSTOM_VERSION = '2.4.1-dev';
 
     /**
      * @var string
