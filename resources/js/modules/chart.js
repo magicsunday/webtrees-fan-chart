e94e196c9c9fbe069aab026a9ea4dcf4a34a0caa
Rico Sonntag <mail@ricosonntag.de>
2023-01-25 11:46:13 +0100

    Update file headers

--- resources/js/modules/chart.js
+++ resources/js/modules/chart.js
@@ -1,5 +1,8 @@
 /**
- * See LICENSE.md file for further details.
+ * This file is part of the package magicsunday/webtrees-fan-chart.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
 
 import * as d3 from "./d3";
--- src/Traits/ModuleChartTrait.php
+++ src/Traits/ModuleChartTrait.php
@@ -1,7 +1,10 @@
 <?php
 
 /**
- * See LICENSE.md file for further details.
+ * This file is part of the package magicsunday/webtrees-fan-chart.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
 
 declare(strict_types=1);
