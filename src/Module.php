eb692e06b6a60b0d8aaf70e6cd0275a4f5cf6a55
Rico Sonntag <mail@ricosonntag.de>
2023-08-28 20:30:27 +0200

    2.6.1-dev

--- src/Module.php
+++ src/Module.php
@@ -63,7 +63,7 @@ class Module extends AbstractModule implements ModuleCustomInterface, ModuleChar
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.6.0';
+    public const CUSTOM_VERSION = '2.6.1-dev';
 
     /**
      * @var string
