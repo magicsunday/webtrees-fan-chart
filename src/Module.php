c7d7ea16e4d5c7688bcf59a7a6ce3968701288af
Rico Sonntag <mail@ricosonntag.de>
2024-10-17 11:53:00 +0200

    Release 2.7.1

--- src/Module.php
+++ src/Module.php
@@ -62,7 +62,7 @@ class Module extends AbstractModule implements ModuleCustomInterface, ModuleChar
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.7.1-dev';
+    public const CUSTOM_VERSION = '2.7.1';
 
     /**
      * @var string
