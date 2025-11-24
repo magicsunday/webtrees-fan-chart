f036dcca5f2091653d52505ea89bd06d11992397
Rico Sonntag <mail@ricosonntag.de>
2025-11-24 07:41:40 +0100

    Fix fan chart global wiring (#149)
    * chore: document playwright browser setup
    
    * refactor: extract tooltip and label renderers (#146)
    
    * fix: restore fan chart global wiring

--- resources/js/modules/index.js
+++ resources/js/modules/index.js
@@ -5,173 +5,119 @@
  * LICENSE file that was distributed with this source code.
  */
 
-import * as d3 from "./lib/d3";
 import Configuration from "./custom/configuration";
-import Chart from "./custom/chart";
+import FanChartRenderer from "./fan-chart-renderer";
 
-/**
- * The application class.
- *
- * @author  Rico Sonntag <mail@ricosonntag.de>
- * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License v3.0
- * @link    https://github.com/magicsunday/webtrees-fan-chart/
- */
+const createConfiguration = (options) => options.configuration instanceof Configuration
+    ? options.configuration
+    : new Configuration(
+        options.labels,
+        options.generations,
+        options.fanDegree,
+        options.fontScale,
+        options.hideEmptySegments,
+        options.showColorGradients,
+        options.showParentMarriageDates,
+        options.showImages,
+        options.showSilhouettes,
+        options.rtl,
+        options.innerArcs,
+    );
+
+const forwardCallback = (callback, handler) => {
+    if (typeof callback === "function") {
+        callback(handler);
+    }
+};
+
+export const createFanChart = (options = {}) => {
+    const configuration = createConfiguration(options);
+    const renderer      = new FanChartRenderer({
+        selector: options.selector,
+        configuration,
+        hierarchyData: options.data,
+        cssFiles: options.cssFiles || [],
+        d3: options.d3,
+    });
+
+    forwardCallback(options.onRender, () => renderer.render());
+    forwardCallback(options.onResize, () => renderer.resize());
+    forwardCallback(options.onCenter, () => renderer.resetZoom());
+    forwardCallback(options.onExport, (type) => renderer.export(type));
+
+    if (!options.onRender) {
+        renderer.render();
+    }
+
+    return renderer;
+};
+
 export class FanChart
 {
-    /**
-     * Constructor.
-     *
-     * @param {string} selector The CSS selector of the HTML element used to assign the chart too
-     * @param {Object} options  A list of options passed from outside to the application
-     *
-     * @param {string[]} options.labels
-     * @param {number}   options.generations
-     * @param {number}   options.fanDegree
-     * @param {number}   options.fontScale
-     * @param {boolean}  options.hideEmptySegments
-     * @param {boolean}  options.showColorGradients
-     * @param {boolean}  options.showParentMarriageDates
-     * @param {boolean}  options.showImages
-     * @param {boolean}  options.showSilhouettes
-     * @param {boolean}  options.rtl
-     * @param {number}   options.innerArcs
-     * @param {string[]} options.cssFiles
-     * @param {Data[]}   options.data
-     */
-    constructor(selector, options)
+    constructor(selector, options = {})
     {
-        this._selector = selector;
-        this._parent   = d3.select(this._selector);
+        this.renderer = createFanChart({
+            ...options,
+            selector,
+        });
 
-        // Set up configuration
-        this._configuration = new Configuration(
-            options.labels,
-            options.generations,
-            options.fanDegree,
-            options.fontScale,
-            options.hideEmptySegments,
-            options.showColorGradients,
-            options.showParentMarriageDates,
-            options.showImages,
-            options.showSilhouettes,
-            options.rtl,
-            options.innerArcs
-        );
-
-        this._cssFiles = options.cssFiles;
-
-        // Set up chart instance
-        this._chart = new Chart(this._parent, this._configuration);
-
-        this.init();
-        this.draw(options.data);
+        this._bindDefaultControls();
     }
 
-    /**
-     * Returns the configuration object.
-     *
-     * @return {Configuration}
-     */
-    get configuration()
+    _bindDefaultControls()
     {
-        return this._configuration;
+        const centerButton = document.querySelector("#centerButton");
+        const exportPng    = document.querySelector("#exportPNG");
+        const exportSvg    = document.querySelector("#exportSVG");
+
+        if (centerButton) {
+            centerButton.addEventListener("click", () => this.center());
+        }
+
+        if (exportPng) {
+            exportPng.addEventListener("click", () => this.export("png"));
+        }
+
+        if (exportSvg) {
+            exportSvg.addEventListener("click", () => this.export("svg"));
+        }
     }
 
-    /**
-     * @private
-     */
-    init()
+    render()
     {
-        // Bind click event on center button
-        d3.select("#centerButton")
-            .on("click", () => this._chart.center());
+        this.renderer.render();
 
-        // Bind click event on export as PNG button
-        d3.select("#exportPNG")
-            .on("click", () => this.exportPNG());
-
-        // Bind click event on export as SVG button
-        d3.select("#exportSVG")
-            .on("click", () => this.exportSVG());
-
-        this.addEventListeners();
+        return this;
     }
 
-    /**
-     * Add event listeners.
-     */
-    addEventListeners()
+    resize()
     {
-        // Listen for fullscreen change event
-        document.addEventListener(
-            "fullscreenchange",
-            () => {
-                if (document.fullscreenElement) {
-                    // Add attribute to the body element to indicate fullscreen state
-                    document.body.setAttribute("fullscreen", "");
-                } else {
-                    document.body.removeAttribute("fullscreen");
-                }
+        this.renderer.resize();
 
-                this._chart.updateViewBox();
-            }
-        );
-
-        // Listen for orientation change event
-        screen.orientation.addEventListener(
-            "change",
-            () => {
-                this._chart.updateViewBox();
-            });
+        return this;
     }
 
-    /**
-     * Updates the chart.
-     *
-     * @param {string} url The update url
-     */
+    center()
+    {
+        this.renderer.resetZoom();
+
+        return this;
+    }
+
+    export(type)
+    {
+        this.renderer.export(type);
+
+        return this;
+    }
+
     update(url)
     {
-        this._chart.update(url);
-    }
+        this.renderer.update(url);
 
-    /**
-     * Draws the chart.
-     *
-     * @param {Object} data The JSON encoded chart data
-     */
-    draw(data)
-    {
-        this._chart.data = data;
-        this._chart.draw();
-    }
-
-    /**
-     * Exports the chart as PNG image and triggers a download.
-     *
-     * @private
-     */
-    exportPNG()
-    {
-        this._chart.svg
-            .export('png')
-            .svgToImage(this._chart.svg, "fan-chart.png");
-    }
-
-    /**
-     * Exports the chart as SVG image and triggers a download.
-     *
-     * @private
-     */
-    exportSVG()
-    {
-        this._chart.svg
-            .export('svg')
-            .svgToImage(
-                this._chart.svg,
-                this._cssFiles,
-                "webtrees-fan-chart-container",
-                "fan-chart.svg"
-            );
+        return this;
     }
 }
+
+export { FanChartRenderer };
+export default FanChart;
