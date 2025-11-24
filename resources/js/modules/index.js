96cefa2e2594799c1bf63102fdc2a342cb977a54
Rico Sonntag <mail@ricosonntag.de>
2025-11-24 07:58:38 +0100

    feat: add callback map for fan chart controls (#151)

--- resources/js/modules/index.js
+++ resources/js/modules/index.js
@@ -30,6 +30,57 @@ const forwardCallback = (callback, handler) => {
     }
 };
 
+const createRendererActions = (renderer) => ({
+    render: () => renderer.render(),
+    resize: () => renderer.resize(),
+    center: () => renderer.resetZoom(),
+    export: (type) => renderer.export(type),
+    exportPNG: () => renderer.export("png"),
+    exportSVG: () => renderer.export("svg"),
+    update: (url) => renderer.update(url),
+});
+
+const registerCallbacks = (callbacks, actions) => {
+    if (!callbacks) {
+        return;
+    }
+
+    forwardCallback(callbacks.onRender, actions.render);
+    forwardCallback(callbacks.onResize, actions.resize);
+    forwardCallback(callbacks.onCenter, actions.center);
+    forwardCallback(callbacks.onExport, actions.export);
+    forwardCallback(callbacks.onExportPNG, actions.exportPNG);
+    forwardCallback(callbacks.onExportSVG, actions.exportSVG);
+    forwardCallback(callbacks.onUpdate, actions.update);
+};
+
+/**
+ * @typedef {Object} FanChartControlCallbacks
+ * @property {(handler: () => void) => void} [onRender] Register a handler that triggers rendering.
+ * @property {(handler: () => void) => void} [onResize] Register a handler that resizes the chart.
+ * @property {(handler: () => void) => void} [onCenter] Register a handler that resets zoom and centers the chart.
+ * @property {(handler: (type: string) => void) => void} [onExport] Register a handler for custom export triggers.
+ * @property {(handler: () => void) => void} [onExportPNG] Register a handler for PNG export.
+ * @property {(handler: () => void) => void} [onExportSVG] Register a handler for SVG export.
+ * @property {(handler: (url: string) => void) => void} [onUpdate] Register a handler to refresh data from a URL.
+ */
+
+/**
+ * @typedef {Object} FanChartOptions
+ * @property {string} selector CSS selector targeting the chart container.
+ * @property {Object} [data] Hierarchy data object for the fan chart.
+ * @property {Configuration} [configuration] Optional configuration instance.
+ * @property {Array<string>} [cssFiles] Additional CSS files to load.
+ * @property {FanChartControlCallbacks} [controls] Callback map for integrating host-provided controls.
+ * @property {Function} [d3] D3 instance for rendering.
+ */
+
+/**
+ * Create a fan chart renderer and wire host-provided controls to its actions.
+ *
+ * @param {FanChartOptions} options Fan chart configuration and control callbacks.
+ * @returns {FanChartRenderer & { actions: ReturnType<typeof createRendererActions> }} Renderer exposing callable actions.
+ */
 export const createFanChart = (options = {}) => {
     const configuration = createConfiguration(options);
     const renderer      = new FanChartRenderer({
@@ -40,14 +91,24 @@ export const createFanChart = (options = {}) => {
         d3: options.d3,
     });
 
-    forwardCallback(options.onRender, () => renderer.render());
-    forwardCallback(options.onResize, () => renderer.resize());
-    forwardCallback(options.onCenter, () => renderer.resetZoom());
-    forwardCallback(options.onExport, (type) => renderer.export(type));
+    const actions = createRendererActions(renderer);
+    const callbacks = options.controls ?? {
+        onRender: options.onRender,
+        onResize: options.onResize,
+        onCenter: options.onCenter,
+        onExport: options.onExport,
+        onExportPNG: options.onExportPNG,
+        onExportSVG: options.onExportSVG,
+        onUpdate: options.onUpdate,
+    };
 
-    if (!options.onRender) {
-        renderer.render();
+    registerCallbacks(callbacks, actions);
+
+    if (!callbacks || !callbacks.onRender) {
+        actions.render();
     }
+
+    renderer.actions = actions;
 
     return renderer;
 };
@@ -60,60 +121,47 @@ export class FanChart
             ...options,
             selector,
         });
-
-        this._bindDefaultControls();
-    }
-
-    _bindDefaultControls()
-    {
-        const centerButton = document.querySelector("#centerButton");
-        const exportPng    = document.querySelector("#exportPNG");
-        const exportSvg    = document.querySelector("#exportSVG");
-
-        if (centerButton) {
-            centerButton.addEventListener("click", () => this.center());
-        }
-
-        if (exportPng) {
-            exportPng.addEventListener("click", () => this.export("png"));
-        }
-
-        if (exportSvg) {
-            exportSvg.addEventListener("click", () => this.export("svg"));
-        }
+        this.actions  = this.renderer.actions;
     }
 
     render()
     {
-        this.renderer.render();
+        this.actions.render();
 
         return this;
     }
 
     resize()
     {
-        this.renderer.resize();
+        this.actions.resize();
 
         return this;
     }
 
     center()
     {
-        this.renderer.resetZoom();
+        this.actions.center();
 
         return this;
     }
 
     export(type)
     {
-        this.renderer.export(type);
+        this.actions.export(type);
 
         return this;
     }
 
     update(url)
     {
-        this.renderer.update(url);
+        this.actions.update(url);
+
+        return this;
+    }
+
+    registerControls(callbacks)
+    {
+        registerCallbacks(callbacks, this.actions);
 
         return this;
     }
