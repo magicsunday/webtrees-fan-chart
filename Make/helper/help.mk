3cfa45e014251eeeab74ce9795451f4cfd3a2096
Rico Sonntag <mail@ricosonntag.de>
2026-04-06 17:32:30 +0200

    Fix missing logo dependency in help and node targets

--- Make/helper/help.mk
+++ Make/helper/help.mk
@@ -4,7 +4,7 @@
 
 .PHONY: help
 
-help:
+help: .logo
 	@echo -e "${FYELLOW}Usage:${FRESET}\n  make [target] ..."
-	@cat $(filter-out %.env, $(MAKEFILE_LIST)) | grep -E '(^[a-zA-Z0-9._-]+:.*##|^#### )' | sed -e 's/\\$$//' | sed -E 's/#### (.+)/ \n${FYELLOW}\1${FRESET}/g' | sed -E '/^[^#].*##/ { s/^([^ ]+):/  ${FGREEN}\1${FRESET}/ }' | column -t -s '##'
+	@cat $(filter-out %.env, $(MAKEFILE_LIST)) | grep -E '(^[a-zA-Z0-9._-]+:.*##|^#### )' | sed -e 's/\\$$//' | sed -e 's/ \.logo//g' | sed -E 's/#### (.+)/ \n${FYELLOW}\1${FRESET}/g' | sed -E '/^[^#].*##/ { s/^([^ ]+):/  ${FGREEN}\1${FRESET}/ }' | column -t -s '##'
 	@echo ""
--- Make/node.mk
+++ Make/node.mk
@@ -6,17 +6,17 @@
 
 .PHONY: install build watch clean
 
-install: ## Install node dependencies.
+install: .logo ## Install node dependencies.
 	@$(COMPOSE_RUN) npm install
 	@echo -e "${FGREEN} ✔${FRESET} Node dependencies installed"
 
-build: ## Build JavaScript bundles.
+build: .logo ## Build JavaScript bundles.
 	@$(COMPOSE_RUN) npm run prepare
 	@echo -e "${FGREEN} ✔${FRESET} JavaScript bundles built"
 
-watch: ## Watch for changes and rebuild automatically.
+watch: .logo ## Watch for changes and rebuild automatically.
 	@$(COMPOSE_RUN) npm run watch
 
-clean: ## Remove node_modules.
+clean: .logo ## Remove node_modules.
 	@$(COMPOSE_RUN) rm -rf node_modules
 	@echo -e "${FGREEN} ✔${FRESET} node_modules removed"
