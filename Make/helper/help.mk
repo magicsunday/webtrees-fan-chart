e7b8b4b38ec9279b4246297a04779b72e20faa60
Rico Sonntag <mail@ricosonntag.de>
2026-04-06 17:34:22 +0200

    Add blank line between usage and targets in help output

--- Make/helper/help.mk
+++ Make/helper/help.mk
@@ -5,6 +5,6 @@
 .PHONY: help
 
 help: .logo
-	@echo -e "${FYELLOW}Usage:${FRESET}\n  make [target] ..."
+	@echo -e "${FYELLOW}Usage:${FRESET}\n  make [target] ...\n"
 	@cat $(filter-out %.env, $(MAKEFILE_LIST)) | grep -E '(^[a-zA-Z0-9._-]+:.*##|^#### )' | sed -e 's/\\$$//' | sed -e 's/ \.logo//g' | sed -E 's/#### (.+)/ \n${FYELLOW}\1${FRESET}/g' | sed -E '/^[^#].*##/ { s/^([^ ]+):/  ${FGREEN}\1${FRESET}/ }' | column -t -s '##'
 	@echo ""
