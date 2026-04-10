4242c3cd5619c88c35e9c345c609d2f105dcecc8
raaspt <alberto.r.a.almeida@gmail.com>
2026-04-10 16:05:45 +0100

    Fix missing death symbol when death place exists without death date (#185)

--- src/Facade/DataFacade.php
+++ src/Facade/DataFacade.php
@@ -283,7 +283,7 @@ class DataFacade
             }
 
             if ($deathPlace !== '') {
-                $lines[] = $deathPlace;
+                $lines[] = Symbols::SYMBOL_DEATH . ' ' . $deathPlace;
             }
 
             return implode("\n", $lines);
